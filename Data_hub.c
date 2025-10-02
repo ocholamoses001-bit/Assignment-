8799b31c21b299f1980c0d6265f64b9652529efd
ocholamoses001-bit <ocholamoses001@gmail.com>
2025-10-01 22:04:01 -0700

    Add files via upload

---
+++ Data_hub.c
@@ -0,0 +1,22 @@
+//Simple hello World program
+/*
+Name:Moses Ouma
+Reg no:PA106/G/28795/25
+) Description: Prorams to display choices and cost of data bundles;
+*/
+#include<stdio.h>
+int main(){
+int choice;
+//Display menu
+printf("enter choice of data\n");
+printf("100MB @ 50KES\n");
+printf("500MB @ 200KES\n");
+printf("1GB @ 350KES\n");
+printf("2GB @ 600KES\n");
+
+printf("enter data choice 1-4");
+scanf("%d", & choice );
+
+
+return 0;
+}
