a315cd633b835d36637b5d669c48bed3633f6a08
ocholamoses001-bit <ocholamoses001@gmail.com>
2025-10-08 23:11:04 -0700

    Add files via upload

---
+++ withdrawal.c
@@ -0,0 +1,22 @@
+//Simple hello World program
+/*
+Name:khamisi Moses Ouma 
+Reg no:PA106/G/28795/25
+) Description: Prorams to show ATM withdrawal 
+*/
+#include<stdio.h>
+int main()
+{
+float withdraw, balance, accountbalance;
+printf("Enter account balance:");
+scanf("%f", & accountbalance);
+printf("Enter amount to be withdrawn:");
+scanf("%f",& withdraw);
+
+scanf("%f",& balance);
+balance =accountbalance - withdraw;
+
+if(balance<=0)
+printf("you have insufficient funds");
+return 0;
+}
